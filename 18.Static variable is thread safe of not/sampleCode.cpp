afc306d4200e937c243a85cb465f4894ea470bb0
Ali Bakhshayesh <ali.bakhshayesh@gmail.com>
2023-09-22 23:45:44 +0330

    static and thread safe

---
+++ 18.Static variable is thread safe of not/sampleCode.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <future>
+using namespace std;
+
+static int val =0;
+//int val =0;
+std::mutex m1;
+void threadFunc()
+{
+	int count = 100000;
+	//m1.lock();
+	while(count--)
+			++val;
+	//m1.unlock();
+}
+int main()
+{
+	std::thread t1(threadFunc);
+	std::thread t2(threadFunc);
+	
+	t1.join();
+	t2.join();
+	
+	cout << "Value is: " << val << endl;
+	return 0;
+}
+
